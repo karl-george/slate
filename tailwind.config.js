2d990268d6e3c744bc74493825a4e57934d651e9
Karl George <karlgeorge87@gmail.com>
2025-02-24 14:46:40 +0100

    Add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        bg: '#181818',
+        accent: '#02E4C0',
+      },
       fontFamily: {
         inter: ['inter', 'sans-serif'],
         poppins: ['Poppins', 'sans-serif'],
